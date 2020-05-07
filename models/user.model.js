4ae91258941a945bb28d7fae8ee6c4a45d3301d7
edwintian <rxchen87@gmail.com>
2020-05-07 16:37:27 +0800

    make userId unique field in user.model

--- models/user.model.js
+++ models/user.model.js
@@ -6,36 +6,37 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    minlength: 3,
+    minlength: 3
   },
   username: {
     type: String,
     required: true,
+    unique: true,
     minlength: 3,
-    lowercase: true,
+    lowercase: true
   },
   password: {
     type: String,
     required: true,
-    minlength: 8,
+    minlength: 8
   },
   firstName: String,
   lastName: String,
   email: {
     type: String,
-    minlength: 3,
-  },
+    minlength: 3
+  }
 });
 
-userSchema.virtual("fullName").get(function () {
+userSchema.virtual("fullName").get(function() {
   return `${this.salutation} ${this.firstName} ${this.lastName}`;
 });
 
-userSchema.virtual("reverseName").get(function () {
+userSchema.virtual("reverseName").get(function() {
   return `${this.lastName}, ${this.firstName}`;
 });
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function() {
   if (this.isModified("password")) {
     const rounds = 10;
     this.password = await bcrypt.hash(this.password, rounds);
